eb0927e0ff0d13eb53bdd8b373a6607c7d882432
kguryanov <kguryanov@gmail.com>
2016-03-05 17:50:55 +0200

    first assignment

---
+++ Week2/my.py
@@ -0,0 +1,13 @@
+# Simple text output
+
+# Store string literal for latter use
+theStr = "You should never underestimate the predictability of stupidity."
+
+# Get padding width for quote 'author'
+padWidth = len(theStr)
+
+# Print quote
+print(theStr)
+
+# Print quote 'author' ) with right padding
+print("-- Bullet Tooth Tony".rjust(len(theStr)))
