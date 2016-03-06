27b3c21100b4dae4f4249ff11b167a3cc99f257f
kguryanov <kguryanov@gmail.com>
2016-03-06 13:28:11 +0200

    payment exersize

--- Week4/conditionals/pay.py
+++ Week4/conditionals/pay.py
@@ -1,18 +1,19 @@
 hrs = raw_input("Enter Hours:")
+rt = raw_input("Enter Rate:")
 worked = 0
-# standard payment
-rate = 10.5
-
-# overtime settings
-overtimeStart = 40
-overtimePay = rate * 1.5
+rate = 0
 
 # Check if input is valid, quit with error code 100 if it is not
 try:
     worked = float(hrs)
+    rate = float(rt)
 except ValueError:
     print "Not a float value"
     quit(100)
+
+# overtime settings
+overtimeStart = 40
+overtimePay = rate * 1.5
 
 if hrs <= 40:
     pay = worked * rate
